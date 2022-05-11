75d02ec0e33966bcd8c0ed43ba40a60b087fed9a
Dante Stevens <danteorionstevens@gmail.com>
2022-05-10 21:36:08 -0400

    coins

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -2,7 +2,7 @@ import OmeggaPlugin, { OL, PS, PC } from 'omegga';
 import { BrickInteraction } from '../omegga-test/omegga';
 
 type Config = { foo: string };
-type Storage = { bar: string };
+type Storage = { bar: string, coinstore: Map<string, number>};
 
 export default class Plugin implements OmeggaPlugin<Config, Storage> {
   omegga: OL;
@@ -16,18 +16,63 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
   }
 
   async init() {
+
+    const coinName: string = 'Onion Coin';
+
     // Write your plugin!
-    this.omegga.on('cmd:test', (speaker: string) => {
-      this.omegga.broadcast(`Hello, ${speaker}!`);
+    this.omegga.on('cmd:count', (speaker: string, target: string) => {
+      let player = Omegga.findPlayerByName(target);
+      Omegga.middlePrint(speaker, `${player.name} has ${getCoins(player.name)} coins.`);
+      
+    });
+
+    /*
+    this.omegga.on('cmd:leaderboard', (speaker: string) => {
+
+    });
+    */
+
+    this.omegga.on('cmd:givecoin', (speaker: string, target: string, quantity: string) => {
+      let source = Omegga.findPlayerByName(speaker);
+      if(source.isHost()){
+        let destination = Omegga.findPlayerByName(target);
+        let num = parseInt(quantity);
+        if(isNaN(num)){
+          Omegga.middlePrint(source, `${num} is not a valid integer.`);
+        } else {
+          giveCoin(destination.name, num);
+          Omegga.middlePrint(source, `You have given ${destination.name} ${coinName} x${num}.`);
+        }
+      }
     });
 
     this.omegga.on('interact', (interaction: BrickInteraction) => {
+      
       if(interaction.message == 'killUser'){
         let player = this.omegga.findPlayerByName(interaction.player.name);
         player.kill();
         Omegga.middlePrint(player.name, "<emoji>dead</>");
       }
     });
+
+    async function giveCoin(target: string, quantity: number){
+      let data = await this.store.get('coinstore');
+      let total = quantity;
+      if(data.has(target)){
+        total += data.get(target);
+      }
+      data.set(target, total);
+      await this.store.set('coinstore', data);
+    }
+
+    async function getCoins(target: string){
+      let data = await this.store.get('coinstore');
+      if(data.has(target)){
+        return data.get(target);
+      } else {
+        return 0;
+      }
+    }
 
     return { registeredCommands: ['test'] };
   }
